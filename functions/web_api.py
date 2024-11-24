97022c2613b6eb83fab5a70be5ae07058dbc2795
Shinichi Yoshinaga <shinichi.yoshinaga@brother.co.jp>
2024-11-24 13:56:10 +0900

    up

--- functions/web_api.py
+++ functions/web_api.py
@@ -2,15 +2,27 @@
 import requests, json
 
 # theMealDBのAPIを利用して、レシピ情報を取得するクラス
-class RecipeAPI:
+class RecipieAPI:
     def __init__(self):
-        self.endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?'
+        self.endpoint_search = 'https://www.themealdb.com/api/json/v1/1/search.php?'
 
+    # 料理一覧を取得
     def get_meal_list(self):
-        data = requests.get(f'{self.endpoint}f=a').json()
-        # JSONデータを整形して表示 
-        formatted_data = json.dumps(data, indent=4, ensure_ascii=False)
-        return formatted_data
+        self.meals_list = []
+        # 先頭文字をaからzまで取得
+        # for i in range(97, 123):
+        for i in range(97, 98):
+            char_search = chr(i)
+            datas = requests.get(f'{self.endpoint_search}f={char_search}').json()
+            # JSONデータを整形して表示
+            formatted_datas = json.dumps(datas, indent=4, ensure_ascii=False)
+            # print(formatted_datas)
+            datas = datas["meals"]
+            # mealsより料理情報を取得
+            for data in datas:
+                self.meals_list.append(data["strMeal"])
+        print(self.meals_list)
+        # return formatted_datas
 
     def get_recipie_detail(self, recipie_id):
         res = requests.get(f'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId={self.applicationid}&categoryId={recipie_id}')
@@ -28,5 +40,5 @@ class RecipeAPI:
 
 
 if __name__ == '__main__':
-    api = RecipeAPI()
+    api = RecipieAPI()
     print(api.get_meal_list())
