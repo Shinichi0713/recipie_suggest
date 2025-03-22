1ab2d66b2161c63a3ad8a526b8dac41a74a92036
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2025-03-22 20:46:02 +0900

    up

--- trial/image_get.py
+++ trial/image_get.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
+import certifi
+import ssl
+
 
 def google_image_search(query, num_images=10):
     query = urllib.parse.quote_plus(query)
@@ -9,8 +12,9 @@ def google_image_search(query, num_images=10):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
-
-    response = requests.get(url, headers=headers, verify=False)
+    path_cert = r"C:\python\Lib\site-packages\certifi\cacert.pem"
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    response = requests.get(url, headers=headers, verify=ssl_context)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'html.parser')
